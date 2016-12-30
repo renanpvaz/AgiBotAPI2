093d58b763403e6bfa5c96d6cde160f44b5235d4
renanpvaz <renanp.vaz@outlook.com>
2016-12-30 13:54:56 -0200

    refatorar

--- server.js
+++ server.js
@@ -1,49 +1,44 @@
 'use strict';
 
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/verificarCPF', function (req,res) {
-    const cpf = req.query.cpf;
-    let customObj = {
-        'perfilCliente': null,
-        'fontePagadora': null,
-        'bancoRecebimento': null,
-        'rendaAproximada': null,
-        'ehCliente': null
-    }
+app.get('/verificarCPF', ({ query }, { json }) => {
+  const cpf = query.cpf;
+  const customObj = {};
 
-    if (cpf === '123') {
-        customObj.perfilCliente = 'Aposentado';
-        customObj.fontePagadora = 'INSS';
-        customObj.bancoRecebimento = 'Bradesco';
-        customObj.rendaAproximada = 'R$ 2.034,55';
-        customObj.ehCliente = true;
-    } else {
-        customObj.ehCliente = false;
-    }
+  if (cpf === '123') {
+    customObj.perfilCliente = 'Aposentado';
+    customObj.fontePagadora = 'INSS';
+    customObj.bancoRecebimento = 'Bradesco';
+    customObj.rendaAproximada = 'R$ 2.034,55';
+    customObj.ehCliente = true;
+  } else {
+    customObj.ehCliente = false;
+  }
 
-    return res.json(customObj);
+  return json(customObj);
 });
 
-app.get('/simulacao', function (req,res) {
-    const valorQuePodePagar = parseFloat(req.query.vqpp);
-    const valorRecalculado = (valorQuePodePagar * 0.85).toFixed(2);
-    const formatter = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-    });
-    let parcelas = [];
+app.get('/simulacao', ({ query }, { json }) => {
+  const parcelas = [];
+  const valorQuePodePagar = parseFloat(query.vqpp);
+  const valorRecalculado = (valorQuePodePagar * 0.85).toFixed(2);
+  const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  });
 
-    for (let i = 0; i <=11; i++){
-        let valorFormatado = formatter.format(valorRecalculado * (i + 1));
-        parcelas[i] = (i+1) + 'x = ' +  valorFormatado;
-    }
+  for (let i = 0; i <= 11; i++){
+    let valorFormatado = formatter.format(valorRecalculado * (i + 1));
+    parcelas[i] = (i + 1) + 'x = ' +  valorFormatado;
+  }
 
-    res.json(parcelas);
+  return json(parcelas);
 });
 
 app.listen(port);
+
 console.log('Server rodando na porta ' + port);
